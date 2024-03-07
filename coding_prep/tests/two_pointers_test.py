a7eb501ffafeee8136de7b04790c29c429fcb57f
Marc Alphonsus <marc.alphonsus@helsing.ai>
2024-03-07 11:10:38 +0000

    housekeeping

--- coding_prep/tests/two_pointers_test.py
+++ coding_prep/tests/two_pointers_test.py
@@ -5,8 +5,9 @@ import pytest
 @pytest.fixture
 def palindrome_data() -> list[tuple[str, bool]]:
     inpt = ["kayak", "hello", "RCAEACAR", "a", "ABCDABCD"]
-    expected = [True, False, False, True,False]
+    expected = [True, False, False, True, False]
     return list(zip(inpt, expected))
+
 
 def test_is_palindrome(palindrome_data: list[tuple[str, bool]]) -> None:
     for data in palindrome_data:
