66bb21c2763814ef2ba296fa87b607ff83a4d77e
marcalph <alphonsus.marc@gmail.com>
2024-03-23 19:03:57 +0100

    solution to circular array loop

--- coding/src/fast_and_slow_pointers.py
+++ coding/src/fast_and_slow_pointers.py
@@ -11,6 +11,8 @@ However, the two-pointer technique is not suitable when:
     2. The problem can be solved with two pointers traversing an array or a linked list at the same pace.
 """
 
+from src.linked_list_node import LinkedListNode
+
 
 def find_square_sum(num):
     _sum = 0
@@ -29,6 +31,57 @@ def is_happy_number(num):
         if slow == fast:  # found the cycle
             break
     return slow == 1  # see if the cycle is stuck on the number '1'
+
+
+def has_cycle(head: LinkedListNode):
+    slow, fast = head, head
+    while fast is not None and fast.next is not None:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:  # found the cycle
+            return True
+    return False
+
+
+def circular_array_has_loop(nums):
+    size = len(nums)
+    for i in range(size):
+        slow = fast = i
+        forward = nums[i] > 0
+
+        while True:
+            slow = next_step(slow, nums[slow], size)
+            if is_not_cycle(nums, forward, slow):
+                break
+
+            fast = next_step(fast, nums[fast], size)
+            if is_not_cycle(nums, forward, fast):
+                break
+
+            fast = next_step(fast, nums[fast], size)
+            if is_not_cycle(nums, forward, fast):
+                break
+
+            if slow == fast:
+                return True
+    return False
+
+
+# A function to calculate the next step
+def next_step(pointer, value, size):
+    result = (pointer + value) % size
+    if result < 0:
+        result += size
+    return result
+
+
+# A function to detect a cycle doesn't exist
+def is_not_cycle(nums, prev_direction, pointer):
+    curr_direction = nums[pointer] >= 0
+    if (prev_direction != curr_direction) or (abs(nums[pointer] % len(nums)) == 0):
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
