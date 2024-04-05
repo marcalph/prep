7690d67e956e458db9168732c90994090617cef7
marcalph <alphonsus.marc@gmail.com>
2024-04-05 20:23:12 +0200

    fractional decimal

--- coding/src/hm.py
+++ coding/src/hm.py
@@ -49,3 +49,36 @@ class RequestLogger:
             self.hm[request]=timestamp
             return True
         return False
+    
+
+def fraction_to_decimal(numerator, denominator):
+    string_result = ""
+    if numerator == 0:
+        return "0"
+    if bool(numerator) ^ bool(denominator):
+        string_result += "-"
+        numerator*=-1
+    
+    hm  = {}
+    remainder = numerator % denominator   
+    string_result += str(numerator//denominator)
+
+    if remainder==0:
+        return string_result
+    else:
+        string_result+="."
+
+    while remainder !=0:
+        hm[str(remainder)] = len(string_result)
+        numerator = 10*remainder
+        string_result+=str(numerator//denominator)
+        remainder = numerator%denominator
+        print(hm)
+        if str(remainder) in hm:
+            append = ")"
+            string_result += append
+            return string_result[:hm[str(remainder)]]+"("+string_result[hm[str(remainder)]:]
+
+    
+    # Replace this placeholder return statement with your code
+    return string_result
