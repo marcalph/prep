239020ca2400a70c8916028d43ab56a6473a411b
marcalph <alphonsus.marc@gmail.com>
2024-04-05 14:03:43 +0200

    add request logger

--- coding/src/hm.py
+++ coding/src/hm.py
@@ -29,3 +29,23 @@ class MyHashMap():
     def remove(self, key):
         # Write your code here
         self.arr[key%self.size]=-1
+
+
+
+
+class RequestLogger:
+    def __init__(self, time_limit):
+        # Initialize your data structure here
+        self.hm = {}
+        self.limit = time_limit
+
+    # This function decides whether the message request should be accepted or rejected
+    def message_request_decision(self, timestamp, request):
+        # Replace this placeholder return statement with your code
+        if request not in self.hm:
+            self.hm[request] = timestamp
+            return True
+        if timestamp-self.hm[request]>=self.limit:
+            self.hm[request]=timestamp
+            return True
+        return False
