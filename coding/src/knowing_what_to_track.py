a2707229cb4c98b8d1ee74b372da7cf43579f0e9
marcalph <alphonsus.marc@gmail.com>
2024-04-24 11:38:28 +0200

    freq count

--- coding/src/knowing_what_to_track.py
+++ coding/src/knowing_what_to_track.py
@@ -7,22 +7,57 @@ Use this under the following conditions:
 """
 from collections import Counter
 
+from collections import Counter
+
 def permute_palindrome(st):
+    """
+    Check if a given string can be rearranged to form a palindrome.
+    """
     cnt = Counter(st)
-    # Replace this placeholder return statement with your code
     return len([v for v in cnt.values() if v%2==1]) <=1
 
 
 
 def is_anagram(str1, str2):
-    if len(str1) != len (str2):
-    # Replace this placeholder return statement with your code
+    """
+    Check if two strings are anagrams of each other.
+    """
+    if len(str1) != len(str2):
         return False
 
-    hm = {} 
+    hm = {}
     for ch in str1:
         hm[ch] = hm.get(ch, 0) + 1
     for ch in str2:
         hm[ch] = hm.get(ch, 0) - 1
     vals = hm.values()
-    return len(list(filter(lambda x:x!=0, vals)))==0 
+    return len(list(filter(lambda x: x != 0, vals))) == 0
+
+
+
+class FreqStack:
+    """
+    A class representing a frequency stack.
+        push(value): Pushes the given value into the stack.
+        pop(): Pops and returns the most frequent element from the stack.
+    """
+    def __init__(self):
+        self.inp = []
+        self.hm = {}
+
+    def push(self, value):
+        self.inp.append(value)
+        self.hm[value] = self.hm.get(value, 0) + 1
+
+    def pop(self):
+        maxfreq = 0
+        for freq in self.hm.values():
+            if freq > maxfreq:
+                maxfreq = freq
+
+        for i, j in enumerate(self.inp[::-1]):
+            if maxfreq == self.hm[j]:
+                self.inp.pop(-(i + 1))
+                self.hm[j] -= 1
+                break
+        return j
