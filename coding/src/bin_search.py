ccf710d55d5e96c5ab569be862abb980c7fc1f9c
marcalph <alphonsus.marc@gmail.com>
2024-05-03 10:17:48 +0200

    test

--- coding/src/bin_search.py
+++ coding/src/bin_search.py
@@ -49,12 +49,12 @@ def binary_search_rotated(nums, target):
       if target==nums[mid]:
         return mid
       if nums[l] <= nums[mid]:
-          if nums[l] <= target and target < nums[mid]:
+          if nums[l] <= target < nums[mid]:
               r = mid - 1 
           else:
               l = mid + 1 
       else:
-          if nums[mid] < target and target <= nums[r]:
+          if nums[mid] < target <= nums[r]:
               l = mid + 1
           else:
               r = mid - 1 
