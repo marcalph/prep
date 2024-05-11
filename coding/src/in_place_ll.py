cbcc222e3008c4564c1887e47bf10b5ac13c47b2
marcalph <alphonsus.marc@gmail.com>
2024-05-11 18:43:27 +0200

    wip linked lists

--- coding/src/in_place_ll.py
+++ coding/src/in_place_ll.py
@@ -30,3 +30,39 @@ def reverse_between(head, left, right):
         next.next = prev.next
         prev.next = next
 
+
+def reverse_linked_list(head, k):
+	previous, current, next = None, head, None
+	for _ in range(k):
+		next = current.next
+		current.next = previous
+		previous = current
+		current = next
+	return previous, current
+
+
+
+
+def reverse_k_groups(head, k):
+    dummy = LinkedListNode(0)
+    dummy.next = head
+    ptr = dummy
+
+    while ptr:    
+        tracker = ptr
+        for i in range(k):
+            if tracker == None:
+                break
+            tracker = tracker.next
+
+        if tracker == None:
+            break
+
+        previous, current = reverse_linked_list(ptr.next, k)
+
+        last_node_of_reversed_group = ptr.next
+        last_node_of_reversed_group.next = current
+        ptr.next = previous
+        ptr = last_node_of_reversed_group
+
+    return dummy.next
