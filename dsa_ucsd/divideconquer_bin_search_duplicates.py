bb85d39e41d3f52fc7fbe49860f590aa3c562c24
marcalph <alphonsus.marc@gmail.com>
2025-03-18 18:16:03 +0100

    divide conquer

---
+++ dsa_ucsd/divideconquer_bin_search_duplicates.py
@@ -0,0 +1,26 @@
+def binary_search(keys, query):
+    # write your code here
+    if keys[0]==query:
+        return 0
+    l, r = 1, len(keys)-1
+    while l<=r:
+        m = (l+r)//2
+        if keys[m]==query and keys[m-1]!=query:
+                return m     
+        elif keys[m]<query:
+            l = m+1
+        else:
+            r=m-1
+    return -1
+
+if __name__ == '__main__':
+    num_keys = int(input())
+    input_keys = list(map(int, input().split()))
+    assert len(input_keys) == num_keys
+
+    num_queries = int(input())
+    input_queries = list(map(int, input().split()))
+    assert len(input_queries) == num_queries
+
+    for q in input_queries:
+        print(binary_search(input_keys, q), end=' ')
---
+++ dsa_ucsd/divideconquer_majority_elt.py
@@ -0,0 +1,36 @@
+from collections import Counter
+def majority_element_naive(elements):
+    cnt = Counter(elements)
+    return int(cnt.most_common(1)[0][1]>len(elements)/2)
+
+
+def majority_element(elements, left, right):
+    if left == right:
+        return -1
+    if left + 1 == right:
+        return elements[left]
+    middle = (left + right) // 2
+    x = majority_element(elements, left, middle)
+    if elements[left:right].count(x) > (right - left)/2:
+        return x
+    x = majority_element(elements, middle, right)
+    if elements[left:right].count(x) > (right - left)/2:
+        return x
+    return -1
+
+
+
+# If a majority exists in the original list, at least one duplicate pair must exist if the list is partioned into pairs. If a duplicate pair of an element does not exist, then  that element can not have more than n/2 copies in the list.
+# After running through the list and discarding any elements not in a duplicate pair, we can take the remaining elements and count their instances in the original list to see if any have >n/2.
+# example:
+# [A, B, B, B, A, C, C, C, B]
+# AB BB AC CC CB
+# B, C
+# count instances of B and C in original list
+
+
+if __name__ == '__main__':
+    input_n = int(input())
+    input_elements = list(map(int, input().split()))
+    assert len(input_elements) == input_n
+    print(majority_element_naive(input_elements))
