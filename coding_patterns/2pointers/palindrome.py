5326afc22cb8016dc8fe823f37d12551cea339ce
marcalph <marcalph@protonmail.com>
2025-12-01 14:25:01 +0100

    lint

--- coding_patterns/2pointers/palindrome.py
+++ coding_patterns/2pointers/palindrome.py
@@ -1,6 +1,6 @@
 def is_palindrome(s: str) -> bool:
     # remove non-alphanumeric chars: ''.join(c for c in s if c.isalnum())
-    s = ''.join(c.lower() for c in s if c.isalnum())
+    s = "".join(c.lower() for c in s if c.isalnum())
     left, right = 0, len(s) - 1
     while left < right:
         if s[left] != s[right]:
