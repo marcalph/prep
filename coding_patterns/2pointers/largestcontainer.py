eb831a59d2f1bc4a084bc3c0fea50962b17d9c5a
marcalph <marcalph@protonmail.com>
2025-12-01 15:49:02 +0100

    add exercise

--- coding_patterns/2pointers/largestcontainer.py
+++ coding_patterns/2pointers/largestcontainer.py
@@ -1,20 +1,15 @@
-def largest_containers(heights: list[int]=[2, 7, 8, 3, 7, 6]) -> int:
-    left, right = 0, len(heights)-1
+def largest_containers(heights: list[int] = [2, 7, 8, 3, 7, 6]) -> int:
+    left, right = 0, len(heights) - 1
     running_max = 0
     while left < right:
-        area = min(heights[left], heights[right]) * (right - left) 
+        area = min(heights[left], heights[right]) * (right - left)
         running_max = max(area, running_max)
-        if heights[left]> heights[right]:
-            right-=1
+        if heights[left] > heights[right]:
+            right -= 1
         else:
-            left+=1
+            left += 1
     return running_max
-
-
-
-
 
 
 if __name__ == "__main__":
     print(largest_containers())
-
