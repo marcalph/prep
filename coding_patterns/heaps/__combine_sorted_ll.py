30f82e3d61212631ece067c8efdb7fcf9bb5dbf6
marcalph <marcalph#protonmail.com>
2025-12-06 15:57:09 +0100

    heap

--- coding_patterns/heaps/__combine_sorted_ll.py
+++ coding_patterns/heaps/__combine_sorted_ll.py
@@ -1,30 +1,31 @@
 from typing import List
-from ds import ListNode
 
-"""
-Definition of ListNode:
+#Definition of ListNode:
 class ListNode:
     def __init__(self, val=None, next=None):
         self.val = val
         self.next = next
-"""
+
+
+import heapq
 
 def combine_sorted_linked_lists(lists: List[ListNode]) -> ListNode:
-    # Write your code here
-    import heapq
     heap = []
-    for ll in lists:
-        node=ll
-        while node:
-            if node.val:
-                heapq.heappush(heap, node.val)
-            node = node.next
-    print(heap)
-    res = ListNode()
-    head = ListNode()
-    current = head
+
+    # Push the head of each list into the heap
+    for i, node in enumerate(lists):
+        if node:
+            heapq.heappush(heap, (node.val, i, node))
+
+    dummy = ListNode(0)
+    current = dummy
+
     while heap:
-        current.val = heapq.heappop(heap)
-        current.next = ListNode()
+        val, i, node = heapq.heappop(heap)
+        current.next = node
         current = current.next
-    return head
+
+        if node.next:
+            heapq.heappush(heap, (node.next.val, i, node.next))
+
+    return dummy.next
