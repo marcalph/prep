c64500a518a5ec76f9a0a6f51ef10c187c51ecfc
marcalph <marcalph#protonmail.com>
2025-12-07 17:46:46 +0100

    trie

--- coding_patterns/trie/search_wild_cards.py
+++ coding_patterns/trie/search_wild_cards.py
@@ -4,43 +4,36 @@ class TrieNode:
         self.is_word = False
 
 
-
 class InsertAndSearchWordsWithWildcards:
     def __init__(self):
         self.root = TrieNode()
-        
 
     def insert(self, word: str) -> None:
         current_node = self.root
         for char in word:
             if char not in current_node.children:
-                current_node.children[char]= TrieNode()
+                current_node.children[char] = TrieNode()
             current_node = current_node.children[char]
         current_node.is_word = True
-
 
     def search(self, word: str) -> bool:
         return self.search_helper(0, word, self.root)
 
-    def search_helper(self, word_index: int, word: str,
-        node: TrieNode) -> bool:
+    def search_helper(self, word_index: int, word: str, node: TrieNode) -> bool:
         for i in range(word_index, len(word)):
             c = word[i]
             # If a wildcard character is encountered, recursively
             # search for the rest of the word from each child node.
-            if c=='.':
+            if c == ".":
                 for child in node.children.values():
-                # If a match is found, return true.
-                    if self. search_helper(i + 1, word, child) :
+                    # If a match is found, return true.
+                    if self.search_helper(i + 1, word, child):
                         return True
                 return False
             elif c in node.children:
-                node= node.children[c]
+                node = node.children[c]
             else:
                 return False
         # After processing the last character, return true if we"ve
         # reached the end of a word.
         return node.is_word
-
-
-
