618abbca1fe4f708c82d722e3127f43b01a3d036
marcalph <marcalph#protonmail.com>
2025-12-08 22:56:48 +0100

    wip

--- coding_patterns/trie/design_trie.py
+++ coding_patterns/trie/design_trie.py
@@ -1,18 +1,25 @@
+from dataclasses import dataclass
+
+@dataclass
+class TrieNode:
+    children: dict= {}
+    is_word:bool = False
+
+
 class Trie:
     def __init__(self):
-        self.children = {}
-        self.is_end = False
+        self.root = TrieNode()
 
     def insert(self, word: str) -> None:
-        node = self
+        node = self.root
         for char in word:
             if char not in node.children:
-                node.children[char] = Trie()
+                node.children[char] = TrieNode()
             node = node.children[char]
         node.is_end = True
 
     def search(self, word: str) -> bool:
-        node = self
+        node = self.root
         for char in word:
             if char not in node.children:
                 return False
@@ -20,7 +27,7 @@ class Trie:
         return node.is_end
 
     def has_prefix(self, prefix: str) -> bool:
-        node = self
+        node = self.root
         for char in prefix:
             if char not in node.children:
                 return False
