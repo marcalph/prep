e9341bdb79ade5c0a8e91835871173bf9a18f52f
marcalph <marcalph#protonmail.com>
2025-12-10 19:12:36 +0100

    test

---
+++ coding_patterns/tree/invert_bt.py
@@ -0,0 +1,22 @@
+
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+def invert_binary_tree(root: TreeNode) -> TreeNode:
+    # Write your code gere
+    if root is None:
+        return None
+
+    # swap children
+    root.left, root.right = root.right, root.left
+
+    # recurse
+    invert_binary_tree(root.left)
+    invert_binary_tree(root.right)
+
+    return root
