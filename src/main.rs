22b69fe743107b5a85339f15b27b251d8f8e3151
marcalph <alphonsus.marc@gmail.com>
2026-01-30 13:44:11 +0100

    add rustlings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Size of a char: {}", std::mem::size_of::<char>());
+    println!("Size of a: {}", "a".len());
+    println!("Size of ß: {}", "ß".len());
+    println!("Size of 国: {}", "国".len());
+    println!("Size of 𓅱: {}", "𓅱".len());
 }
